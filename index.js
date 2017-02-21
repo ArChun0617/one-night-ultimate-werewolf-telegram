2d3ff5f476058f218de451ce0a65fc9e738469de
ArChun0617 <raphaello.work@gmail.com>
2017-02-21 17:13:48 +0800

    Revised testing function Line: 9 - On text with /w, eval() the rest Line: 23 - On text with /b, send a inline button Line: 41 - Inline button callback queries Line: 47 - Inline query by @bot, then you will have the result without send **** This will trigger ajax cache, not repeatly call

--- index.js
+++ index.js
@@ -1,20 +1,86 @@
-var slackAPI = require('slackbotapi');
-var Game = require('./game');
+var TelegramBot = require('node-telegram-bot-api');
+var token = '331592410:AAHy9uA7PLWBHmIcNcyNt78hT6XLarrOjHM';
 
-var CMD_PREFIX = '!w';
-
-var token = '' || process.env.SLACK_API_TOKEN;
-
-var slack = new slackAPI({
-    'token': token,
-    'logging': true,
-    'autoReconnect': true
-});
+var bot = new TelegramBot(token, {polling: true});
 
 var games = {};
 var registry = {};
 
-slack.on('message', function (message) {
+// On text with /w, eval() the rest
+bot.onText(/\/w (.+)/, function (msg, match) {
+	//Command Message Handler, Only cater message start with /w	
+    var chatId = msg.chat.id; //User ID
+    var resp = match[1].replace(/[^-()\d/*+.]/g, '');	//Escape the string
+	
+	console.log(chatId + ":" + resp);
+    resp = 'Result: ' + eval(resp);
+	
+	//Send the message
+    bot.sendMessage(chatId, resp);
+});
+
+// On text with /b, send a inline button
+bot.onText(/\/b (.+)/, function (msg, match) {
+    var chatId = msg.chat.id; //User ID
+	
+	var options = {
+	  reply_markup: JSON.stringify({
+		inline_keyboard: [
+		  [{ text: 'Some button text 1', callback_data: '1' }, { text: 'Some button text 2', callback_data: '2' }, { text: 'Some button text 3', callback_data: '3' }]
+		]
+	  })
+	};
+	console.log(chatId + ":" + options);
+	
+	bot.sendMessage(msg.chat.id, 'Some text giving three inline buttons', options).then(function (sended) {
+	  // `sended` is the sent message.
+	});
+});
+
+// Inline button callback queries
+bot.on('callback_query', function (msg) {
+  console.log(msg); // msg.data refers to the callback_data
+  bot.answerCallbackQuery(msg.id, 'Ok, here ya go!');
+});
+
+// Inline query is @bot , then you will have the result without send **** This will trigger ajax cache, not repeatly call
+bot.on("inline_query", function(para) {
+	var rtn = [];
+	
+	console.log(para);
+	
+	if (para.query == "a")
+	{
+		rtn = [
+			{
+			  type: "article",
+			  id: "a1",
+			  title: "Hello world A1",
+			  input_message_content: {
+				message_text: "A1 Hello, world! This was sent from my super cool inline bot."
+			  }
+			}
+		  ];
+	}
+	else
+	{
+		rtn = [
+			{
+			  type: "article",
+			  id: "a2",
+			  title: "Hello world A2",
+			  input_message_content: {
+				message_text: "A2 Hello, world! This was sent from my super cool inline bot."
+			  }
+			}
+		  ];
+	}
+	
+  bot.answerInlineQuery(para.id, rtn);
+});
+ 
+/*
+bot.onText(/\/gamestart/, function (message) {
   if (message.text.startsWith(CMD_PREFIX + ' ')) {
     var channel = message.channel;
     var parts = message.text.split(' ');
@@ -114,3 +180,4 @@ slack.on('message', function (message) {
     }
   }
 });
+*/
